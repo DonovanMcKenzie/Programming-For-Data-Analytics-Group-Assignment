79decc0b4d3b6c792d86c755d28adb8ec39e0921
Donovan McKenzeie <mckenziedonovan80@gmail.com>
2025-04-16 18:50:04 -0500

    added some images and other resource packs to use

--- dogdash/dogdash.py
+++ dogdash/dogdash.py
@@ -5,21 +5,31 @@ pygame.init()
 #initializes pygames modules for actual game functions
 
 screen_width = 1500
-screen_height = 754
+screen_height = 720
 #defines screen measurements
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Animal Adventure')
 #constructs the screen, names the window
 
+#gamevariables below credits to AxulArt @ https://axulart.itch.io/dirt-grass-2d-platform-tileset-ver-2
+grasscube = pygame.image.load('dd_assets/grassblock.png')
+dirtcube = pygame.image.load('dd_assets/rawdirt.png')
+
+
 runtime = 1
 #run var to control game execution
 
+lvlbg = pygame.image.load('dd_assets/mainlvlbg.png')
+#loaded images above   
+
+
 while runtime == 1:
-#gameloop
+#gameloop 
     
-#loaded images above    
-    
+    screen.blit(lvlbg, (0,0))
+    #here we draw the bg image on screen
+      
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runtime = 0
