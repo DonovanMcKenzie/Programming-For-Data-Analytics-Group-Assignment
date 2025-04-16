64f708da4c74f4ca1eeb26ea44ef2ebc4c4a676c
kam-0-cpu <1khamobro@gmail.com>
2025-04-16 12:07:47 -0500

    Update snakes_and_ladders.py

--- game_1/snakes_and_ladders.py
+++ game_1/snakes_and_ladders.py
@@ -30,9 +30,6 @@ player_name = ""  # Player name will be set by user input
 snakes = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
 ladders = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
 
-# Initialize sounds
-dice_roll_sound = pygame.mixer.Sound("dice_roll.wav")  # Add your dice roll sound file here
-
 def draw_board():
     screen.fill(WHITE)
     number = 1
