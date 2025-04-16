e57be25c16f5dc44f9a4d00b290b1d124d3f65c5
Donovan McKenzeie <mckenziedonovan80@gmail.com>
2025-04-15 19:14:20 -0500

    Added the screen for the main menu, the gameloop and the exit clause

--- main_menu/main_menu.py
+++ main_menu/main_menu.py
@@ -3,3 +3,25 @@
 
 import pygame
 pygame.init()
+
+screen_width = 1500
+screen_height = 820
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+runtime = 1
+#gameloop runtime variable
+
+while runtime == 1:
+#gameloop
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runtime = 0
+    #exit code definition
+    
+    pygame.display.update()
+    #screen upkeep
+    
+pygame.quit()
+#exit code execution
