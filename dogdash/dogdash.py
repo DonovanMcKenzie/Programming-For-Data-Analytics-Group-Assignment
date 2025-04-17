5923738ed1de81cdf34189e0ec5f9e3b4e011fb6
Donovan McKenzeie <mckenziedonovan80@gmail.com>
2025-04-16 20:43:19 -0500

    more additions to the dogdash folder and a notes txt file for obvious reasons

--- dogdash/dogdash.py
+++ dogdash/dogdash.py
@@ -16,7 +16,7 @@ pygame.display.set_caption('Animal Adventure')
 tile_size = 50
 
 def draw_grid():
-	for line in range(0, 20):
+	for line in range(0, 30):
 		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
 		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
